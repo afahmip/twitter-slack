b3bd5f07db7e8ad2b17034651bb71302e445daf0
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2019-05-25 00:36:49 +0700

    Create base Navbar

---
+++ src/components/Dashboard/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import Navbar from './Navbar'
+
+function Dashboard(props) {
+  return (
+    <Navbar />
+  )
+}
+
+Dashboard.propTypes = {
+  children: PropTypes.node.isRequired
+}
+
+export default Dashboard
---
+++ src/containers/TimelinePage/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Dashboard from 'components/Dashboard'
+
+function TimelinePage(props) {
+  return (
+    <Dashboard />
+  )
+}
+
+TimelinePage.propTypes = {
+
+}
+
+export default TimelinePage;
